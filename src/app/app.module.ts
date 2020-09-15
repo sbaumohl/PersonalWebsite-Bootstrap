65e0dc806242a94659c87b41a129eee4acb18564
sam <sbbaumo@yahoo.com>
2020-09-15 10:03:30 -0500

    2.1
    Add more Whitespace
    
    
    Performance tweaks
    
    
    defer
    
    
    2.2
    
    
    remove backup, maybe switch?
    
    
    meta description
    
    
    2.3
    
    
    async styles
    
    2.4
    
    
    reverts and padding
    
    
    whoops
    
    
    Updated Skills, outlined contact links
    
    remove trends, upload pgp
    
    
    2.5
    
    
    pgp redirect + button align
    
    
    Cors all the things
    
    
    cleaning up
    
    
    CONVERSION to Angular
    
    because I'm lazy
    Create deno.yml
    update workflow...
    
    
    whoops
    
    
    add prod

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
+
+// Third party modules
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, fab);
+  }
+}
