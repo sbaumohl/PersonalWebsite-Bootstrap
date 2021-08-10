b02928b78441ed9702c9c6a5a48d9693cfea768e
Sam B <28550422+MeMeBigProgrammer@users.noreply.github.com>
2021-08-09 20:46:59 -0500

    College Update (#2)
    * remove debate, background; simplify layout and colors
    
    * color and css updates
    
    * flex and spacing
    
    * Update angular.json, app.component.html, and 3 more files...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,95 +1,102 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 // import { fa } from '@fortawesome/free-solid-svg-icons';
-import { faHtml5, faCss3Alt, faJava, faUbuntu, faPython, faJs, faAngular, faNodeJs } from '@fortawesome/free-brands-svg-icons';
+import {
+  faHtml5,
+  faCss3Alt,
+  faJava,
+  faUbuntu,
+  faPython,
+  faJs,
+  faAngular,
+  faNodeJs,
+  faReact,
+  faGithub,
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { faKey, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'sambaumohl-com';
-
-  debateCases = [
-    {
-      title: "Public Forum Beedev Meme Casefile",
-      time: "November-December 2018",
-      resolution: "The United States federal government should impose price controls on the pharmaceutical industry.",
-      caseUrl: "../assets/debate/beedev/beedev2.docx",
-      checksumUrl: "../assets/debate/beedev/checksum.txt"
-    },
-    {
-      title: "Lincoln-Douglas Casefile",
-      time: "September-October 2019",
-      resolution: "Universities ought not use standardized tests for undergraduate admissions decisions.",
-      caseUrl: "../assets/debate/standardized/CollegeBoardOutfmymind.docx",
-      checksumUrl: "../assets/debate/standardized/checksum.txt"
-    },
-    {
-      title: "Lincoln-Douglas Casefile",
-      time: "November-December 2019",
-      resolution: "The United States ought to eliminate subsidies for fossil fuels.",
-      caseUrl: "../assets/debate/fossil-fuels/CoalBad.docx",
-      checksumUrl: "../assets/debate/fossil-fuels/checksum.txt"
-    },
-    {
-      title: "Lincoln-Douglas Casefile",
-      time: "March 2020",
-      resolution: "Predictive Policing is injust.",
-      caseUrl: "../assets/debate/ppolicing/policing.docx",
-      checksumUrl: "../assets/debate/ppolicing/checksum.txt"
-    }
-  ]
+  title = "sambaumohl-com";
 
   skills = [
     {
       icon: faHtml5,
-      name: "HTML"
+      name: "HTML",
     },
     {
       icon: faCss3Alt,
-      name: "CSS"
+      name: "CSS",
     },
     {
       icon: faJs,
-      name: "Javascript"
+      name: "Javascript",
     },
     {
       icon: faJava,
-      name: "Java"
+      name: "Java",
     },
     {
       icon: faPython,
-      name: "Python"
+      name: "Python",
     },
     {
       icon: faUbuntu,
-      name: "Linux"
-    },
-    {
-      icon: faAngular,
-      name: "Angular"
+      name: "Linux",
     },
     {
       icon: faNodeJs,
-      name: "Node.js"
-    }
-  ]
+      name: "Node.js",
+    },
+    {
+      icon: faAngular,
+      name: "Angular",
+    },
+    { icon: faReact, name: "React" },
+  ];
 
   resumeProjects = [
     {
       title: "MyMICDS.net",
       description: "An all-in-one platform for MICDS student life.",
       projectUrl: "https://mymicds.net",
-      sourceUrl: "https://github.com/MyMICDS/"
-    }
-    ,
+      sourceUrl: "https://github.com/MyMICDS/",
+    },
     {
       title: "BYO Style-Based GAN",
-      description: "A simple implementation of Stylegan using PyTorch.",
-      sourceUrl: "https://github.com/MeMeBigProgrammer/BYO-GAN"
-    }
-  ]
+      description: "A simple implementation of StyleGAN using PyTorch.",
+      sourceUrl: "https://github.com/MeMeBigProgrammer/BYO-GAN",
+    },
+  ];
 
+  headerLinks = [
+    {
+      icon: faGithub,
+      link: "https://github.com/MeMeBigProgrammer",
+      tooltip: "MeMeBigProgrammer",
+    },
+    { icon: faKey, link: "assets/pgp/key.txt", tooltip: "PGP Public Key" },
+    {
+      icon: faEnvelope,
+      link: "mailto:mail@sambaumohl.me",
+      tooltip: "mail@sambaumohl.me",
+    },
+    {
+      icon: faLinkedin,
+      link: "https://www.linkedin.com/in/sam-baumohl09",
+      tooltip: "LinkedIn Profile",
+    },
+    {
+      icon: faInstagram,
+      link: "https://www.instagram.com/sam_b2327",
+      tooltip: "@sam_b2327",
+    },
+  ];
 }
