f74c703be41183a19c0a1c60cf05b9f967e9911b
Sam B <28550422+MeMeBigProgrammer@users.noreply.github.com>
2021-10-10 23:17:52 -0500

    Next+react (#3)
    * init
    
    * header complete
    
    * more updates
    
    * final
    
    * switch
    
    * workflow
    
    Co-authored-by: Sam B <mail@sambaumohl.me>

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-<!DOCTYPE html>
-<html lang="en" style="height: 100%">
+import Head from "next/head";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { skills, resumeProjects, headerLinks } from "./../lib/resume";
+import TitleHeader from "../components/Title/Title";
+
+const header = (
   <head>
     <meta charset="utf-8" />
     <meta
@@ -12,28 +17,33 @@
     <link
       rel="apple-touch-icon"
       sizes="180x180"
-      href="assets/favicons/apple-touch-icon.png"
+      href="/favicons/apple-touch-icon.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="32x32"
-      href="assets/favicons/favicon-32x32.png"
+      href="/favicons/favicon-32x32.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="16x16"
-      href="assets/favicons/favicon-16x16.png"
+      href="/favicons/favicon-16x16.png"
     />
-    <link rel="manifest" href="assets/favicons/site.webmanifest" />
+    <link rel="manifest" href="/favicons/site.webmanifest" />
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css"
     />
   </head>
+);
 
-  <body class="h-100" style="background-color: #18191a">
-    <app-root></app-root>
-  </body>
-</html>
+export default function Home() {
+  return (
+    <>
+      {header}
+      <TitleHeader></TitleHeader>
+    </>
+  );
+}
