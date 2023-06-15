376b2820469f99ce96e3c9986b39fafe26c5f9d7
sbaumohl <sbaumohl@wisc.edu>
2023-06-15 12:47:25 -0500

    trying astro

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-import Link from "next/link";
-import FrontPage from "../components/FrontPage/FrontPage";
-
-export default function Home() {
-  return (
-    <FrontPage></FrontPage>
-  );
-}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies, global-require */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}',
+    './node_modules/astro-boilerplate-components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        intelonemono: ['"Intel One Mono"', 'mono']
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
+};
