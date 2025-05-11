2c16a269e72a0e5d0e1e056315335a493d608684
Sam Baumohl <28550422+sbaumohl@users.noreply.github.com>
2025-05-11 18:52:22 -0500

    Nash (#22)
    * quick fixes
    
    * arrows
    
    * use g, not svg
    
    * more UI stuff
    
    * updates, latex
    
    * more work
    
    * regret
    
    * x

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,30 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://baumohl.dev",
-  integrations: [mdx({
-    optimize: true,
-  }), sitemap(), tailwind(), icon(), playformCompress()],
+  integrations: [
+    mdx({
+      optimize: true,
+    }),
+    sitemap(),
+    tailwind(),
+    icon(),
+    playformCompress(),
+    react(),
+  ],
   redirects: {
-    '/blog': '/',
-  }
+    "/blog": "/",
+  },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
